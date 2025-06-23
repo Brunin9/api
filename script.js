c940dce4070c8640c4e814920bd93fb7523991bc
Brunin9 <brubor2008@gmail.com>
2025-06-23 10:59:56 -0300

    ok

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-import {useEffect, useState} from 'react';
-
-export default function App() {
-    const output = document.getElementById("otp")
-  const [data, setData] = useState("");
-  const getData = async () => {
-    try {
+async function App() {
+  const output = document.getElementById("output")
       const resp = await fetch('https://api.sampleapis.com/futurama/info');
       const json = await resp.json();
-      setData(json);
-    } catch (err) {
-      setData(err.message);
-    }
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  return (
     output.textContent= JSON.stringify(json, null, 2)
-  )
+  
 }
 
 window.onload = App();
